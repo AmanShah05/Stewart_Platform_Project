c5c1d339cc3c38da5488f92b0d3a0ca15554ff9b
Joshua Hirji-Gliatis <jahg901@gmail.com>
2024-11-12 19:44:36 -0500

    made platformAngle cooler and updated gitignore

--- 380InverseKin.cpp
+++ 380InverseKin.cpp
@@ -6,7 +6,6 @@ using namespace std;
 double nmag, nz, nx_b, nx_c;  //magnitude and z component of the normal vector
 double x, y, z;   //generic variables for the components of legs A, B, and C
 double mag;       //generic magnitude of the leg vector
-double platform_angle;
 double angle;     //generic angle for legs A, B, and C
 double PI = 3.141592653589793;
 double phi_a, phi_b, phi_c;
@@ -16,30 +15,42 @@ double phi_a, phi_b, phi_c;
 #define B 1
 #define C 2
 
-double platform_theta(int leg, double nx, double ny) { 
-  //create unit normal vector
-  nmag = sqrt(pow(nx, 2) + pow(ny, 2) + 1);  //magnitude of the normal vector
-  nx /= nmag;
-  ny /= nmag;
-  nz = 1 / nmag;
-  double check = pow(nx, 2) + pow(ny, 2) + pow(nz, 2);
-  cout << "Check nmag :" << check << endl;
+double* unitVec(double propX, double propY) {
+  double magn = sqrt(pow(propX, 2) + pow(propY, 2) + 1); //magnitude
 
+  double* vec = new double[3];
+  vec[0] = propX/magn;
+  vec[1] = propY/magn;
+  vec[2] = 1/magn;
+
+  double check = pow(vec[0], 2) + pow(vec[1], 2) + pow(vec[2], 2);
+  cout << "Check unit vector magnitude: " << check << endl;
+
+  return vec;
+}
+
+double platformAngle(int leg, double vec[]) { 
+  //take normal vector components
+  double nx = vec[0], ny = vec[1], nz = vec[2];
+  
+  //determine normal radial component based on chosen leg
+  double nr;
   switch (leg) {
     case A:
-      platform_angle = atan(-nx/nz);
+      nr = nx;
       break;
     case B:
-      nx_b = (sqrt(3)/2)*ny-(1/2)*nx;
-      platform_angle = atan(-nx_b/nz);
+      nr = sqrt(0.75)*ny-0.5*nx;
       break;
     case C:
-      nx_c = (-sqrt(3)/2)*ny-(1/2)*nx;
-      platform_angle = atan(-nx_c/nz);
+      nr = -sqrt(0.75)*ny-0.5*nx;
       break;
   }
-  return platform_angle * ( 180 / PI);
+
+  //return platform angle of incline for chosen leg, in degrees
+  return atan(-nr/nz) * (180/PI);
 }
+
 
 // double leg_theta(int leg, double nx, double ny) { 
 //   phi_a = platform_theta(0,nx,ny);
@@ -54,12 +65,13 @@ int main(){
   double ny = 0.342;
 
   double pos[3] = {0, 0, 0};  // Define pos as an array with three elements
-  double testA = platform_theta(A , 0.3, 0);
-  double testB = platform_theta(B , 0.3, 0);
-  double testC = platform_theta(C , 0.3, 0);
+  double* vec = unitVec(0.3, 0.3);
+
+  double testA = platformAngle(A , vec);
+  double testB = platformAngle(B , vec);
+  double testC = platformAngle(C , vec);
 
   cout << testA << " " << testB << " " << testC ;
-  cout << "hi does committing work";
 }
 
 
